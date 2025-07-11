9edb3d03ed1e780bc6800d05cb7270b47e06e85c
codersjj <shanewestlife@outlook.com>
2025-07-11 22:55:29 +0800

    feat: JavaScript Refresher Section 1: Welcome To The Course 12. JavaScript Refresher

---
+++ mycode.js
@@ -0,0 +1,14 @@
+const myVar = 'hi'
+// myVar = "hello";
+console.log(myVar)
+console.log(typeof myVar)
+
+function myFunc() {
+  console.log('This is myFunc')
+}
+
+myFunc()
+myFunc()
+myFunc()
+myFunc()
+myFunc()
