57d4fd047cdd27c7d8ea0302be547dc4423d39df
codersjj <shanewestlife@outlook.com>
2025-07-13 20:22:37 +0800

    feat: Adding Connect Functionality Section 2: HTML TS Buy Me A Coffee 5. Adding Connect Functionality

--- Section_2_HTML_TS_Buy_Me_A_Coffee/html-ts-coffee-cu/index-js.js
+++ Section_2_HTML_TS_Buy_Me_A_Coffee/html-ts-coffee-cu/index-js.js
@@ -1,1 +1,18 @@
-console.log("hi");
+import { createWalletClient, custom } from 'https://esm.sh/viem'
+
+const connectButton = document.getElementById('connectButton')
+
+async function connect() {
+  if (typeof window.ethereum === 'undefined') {
+    connectButton.innerHTML = 'Please install MetaMask'
+  } else {
+    const walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    })
+
+    await walletClient.requestAddresses()
+    connectButton.innerHTML = 'Connected'
+  }
+}
+
+connectButton.onclick = connect
