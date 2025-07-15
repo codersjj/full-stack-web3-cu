0ec6a2cedb3a2bb105edc388851bb44a0f591360
codersjj <shanewestlife@outlook.com>
2025-07-15 13:13:49 +0800

    feat: Simulations Section 2: HTML TS Buy Me A Coffee 9. Simulations

--- Section_2_HTML_TS_Buy_Me_A_Coffee/html-ts-coffee-cu/index-js.js
+++ Section_2_HTML_TS_Buy_Me_A_Coffee/html-ts-coffee-cu/index-js.js
@@ -2,7 +2,10 @@ import {
   createWalletClient,
   custom,
   createPublicClient,
+  parseEther,
+  defineChain,
 } from 'https://esm.sh/viem'
+import { contractAddress, abi as coffeeABI } from './constants-js.js'
 
 const connectButton = document.getElementById('connectButton')
 const fundButton = document.getElementById('fundButton')
@@ -31,15 +34,46 @@ async function fund() {
     const walletClient = createWalletClient({
       transport: custom(window.ethereum),
     })
-    await walletClient.requestAddresses()
+    const [connectedAccount] = await walletClient.requestAddresses()
 
     const publicClient = createPublicClient({
       transport: custom(window.ethereum),
     })
-    // await publicClient.simulateContract({
-    //   // address: ???
-    // })
+    console.log('parseEther(amount):', parseEther(amount))
+
+    const currentChain = getCurrentChain(walletClient)
+
+    const { result } = await publicClient.simulateContract({
+      address: contractAddress,
+      abi: coffeeABI,
+      functionName: 'fund',
+      account: connectedAccount,
+      chain: currentChain,
+      value: parseEther(amount),
+    })
+    console.log('🚀 ~ fund ~ result:', result)
   }
+}
+
+async function getCurrentChain(client) {
+  const chainId = await client.getChainId()
+  console.log('Current chain ID:', chainId)
+  const currentChain = defineChain({
+    id: chainId,
+    name: 'Custom Chain',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ether',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: {
+        http: ['http://localhost:8545'],
+      },
+    },
+  })
+
+  return currentChain
 }
 
 connectButton.onclick = connect
