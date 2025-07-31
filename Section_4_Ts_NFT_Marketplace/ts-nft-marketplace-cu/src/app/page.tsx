cb2ba965407464e5fa8e7ae1a5f1d5bfc0df510e
codersjj <shanewestlife@outlook.com>
2025-07-31 21:41:17 +0800

    feat: Compliance API In A Component Section 4: Ts NFT Marketplace 13. Compliance API In A Component

--- Section_4_Ts_NFT_Marketplace/ts-nft-marketplace-cu/src/app/page.tsx
+++ Section_4_Ts_NFT_Marketplace/ts-nft-marketplace-cu/src/app/page.tsx
@@ -1,22 +1,52 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { useAccount } from "wagmi"
 import RecentlyListedNFTs from "@/components/RecentlyListed"
 
 export default function Home() {
-    const { isConnected } = useAccount()
+  const { isConnected, address } = useAccount()
 
-    return (
-        <main>
-            {!isConnected ? (
-                <div className="flex items-center justify-center p-4 md:p-6 xl:p-8">
-                    Please connect a wallet
-                </div>
-            ) : (
-                <div className="flex items-center justify-center p-4 md:p-6 xl:p-8">
-                    <RecentlyListedNFTs />
-                </div>
-            )}
-        </main>
-    )
+  const [isCompliant, setIsCompliant] = useState(true)
+
+  useEffect(() => {
+    if (address) {
+      checkCompliance()
+    }
+  }, [address])
+
+  async function checkCompliance() {
+    if (!address) return
+
+    const response = await fetch("/api/compliance", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ address }),
+    })
+
+    const result = await response.json()
+
+    const { success, isApproved } = result
+    console.log("🚀 ~ checkCompliance ~ success, isApproved:", success, isApproved)
+
+    setIsCompliant(success && isApproved)
+  }
+
+  return (
+    <main>
+      {!isConnected ? (
+        <div className="flex items-center justify-center p-4 md:p-6 xl:p-8">
+          Please connect a wallet
+        </div>
+      ) : isCompliant ? (
+        <div className="flex items-center justify-center p-4 md:p-6 xl:p-8">
+          <RecentlyListedNFTs />
+        </div>
+      ) : (
+        <div className="flex justify-center items-center m-4 text-xl">You are denied!</div>
+      )}
+    </main>
+  )
 }
